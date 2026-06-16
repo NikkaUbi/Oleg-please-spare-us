765286ffa6723ab9885e0c975b47afea4060f26f
NikkaUbi <155818445+NikkaUbi@users.noreply.github.com>
2026-06-16 15:28:09 +0400

    raghacebs vamtavreb

--- orbitdif.m
+++ orbitdif.m
@@ -1,8 +1,15 @@
 clear;close;clc;
 
 C = 1;
-a = @(r,t) C*r/norm(r)^3;
+C*r/abs(r)^3;
 
 function dxdt = orbitdif(t,X)
 
+  dxdt = zeros[1,4];
 
+  dxdt(1) = X(3);
+  dxdt(2) = X(4);
+  dxdt(3) = C*X(1)/abs(X(1))^3;
+  dxdt(4) = C*X(2)/abs(X(2))^3;
+
+end
